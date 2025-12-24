a6ec6fa553c117cd6a248b66155dcc886babedfc
Lenovo <madhulakhinana@gmail.com>
2025-12-24 23:47:32 +0530

    changes for test upload

--- src/main/java/com/test/myapp/controller/apiclass.java
+++ src/main/java/com/test/myapp/controller/apiclass.java
@@ -8,9 +8,13 @@ import com.test.myapp.service.regservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @RestController
 @RequestMapping("/service")
+@CrossOrigin(origins = "*")
 public class apiclass {
 
     @Autowired
@@ -29,6 +33,26 @@ public class apiclass {
     public String registration(@RequestBody Registration registration){
         String regresp= regservice.regb();
         return regresp;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> getFile(@RequestParam("file")MultipartFile file) throws Exception{
+
+        if(file.isEmpty()){
+            return ResponseEntity.badRequest().body("Upload One File");
+        }
+
+        String currentDir = System.getProperty("user.dir");
+        File uploaddir = new File(currentDir,"/upload");
+
+        if(!uploaddir.exists()){
+        uploaddir.mkdir();
+        }
+
+        File uploadhere= new File(uploaddir, file.getOriginalFilename());
+        System.out.println("uploadhere"+uploadhere);
+        file.transferTo(uploadhere);
+        return ResponseEntity.ok("File uploaded succesfully " + file.getOriginalFilename());
     }
 
 
